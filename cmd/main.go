2bd3c57bd8bed9a8bf589bd5a17de08daaef4366
Hope1esss <leonf2005@gmail.com>
2024-07-09 23:24:28 +0300

    final fix

--- cmd/main.go
+++ cmd/main.go
@@ -1,33 +1,12 @@
 package main
 
 import (
-	"context"
+	"github.com/Hope1esss/pet-app/internal/api"
 	"github.com/Hope1esss/pet-app/internal/config"
 	"github.com/Hope1esss/pet-app/internal/handler"
 	"log"
-	"net/http"
 	"os"
-	"time"
 )
-
-type Server struct {
-	httpServer *http.Server
-}
-
-func (s *Server) Run(port string, handler http.Handler) error {
-	s.httpServer = &http.Server{
-		Addr:           ":" + port,
-		Handler:        handler,
-		MaxHeaderBytes: 1 << 20,
-		ReadTimeout:    10 * time.Second,
-		WriteTimeout:   10 * time.Second,
-	}
-
-	return s.httpServer.ListenAndServe()
-}
-func (s *Server) Shutdown(c context.Context) error {
-	return s.httpServer.Shutdown(c)
-}
 
 //	@title			Pet-app API
 //	@version		1.0
@@ -43,7 +22,7 @@ func (s *Server) Shutdown(c context.Context) error {
 func main() {
 	db := config.InitConfig()
 	app := handler.InitApp(db)
-	srv := new(Server)
+	srv := new(api.Server)
 	if err := srv.Run(os.Getenv("SERVER_PORT"), app.InitRoutes()); err != nil {
 		log.Fatalf("Ошибка при запуске http сервера: %s", err.Error())
 	}
